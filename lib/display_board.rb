4919446094097ac8761420ea73e30093fd1aeb10
Tom Storm <tstorm@invoca.com>
2017-12-28 02:50:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,9 @@
   def display_board
     puts "   ""|""   ""|""   "
     puts "-----------"
+    puts "   ""|""   ""|""   "
+    puts "-----------"
+    puts "   ""|""   ""|""   "
   end
 
   display_board
